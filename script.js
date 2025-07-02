15316a5c26442a70b2afe72ee97e6aa53d942711
Car Price <gravipatel@gmail.com>
2025-07-02 19:38:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1Ps-g1wRp1Q1_i6lx9_u9zBaSQ61S-mvsKB5OKcZLTms/export?format=csv';
+
+fetch(SHEET_CSV_URL)
+  .then(res => res.text())
+  .then(csv => {
+    const rows = csv.split('\n');
+    const data = rows.slice(1);
+    const tbody = document.querySelector('tbody');
+
+    data.forEach(row => {
+      const cols = row.split(',');
+      if (cols.length < 11) return;
+
+      const [brandImg, modelImg, brand, model, exShow, rto, insurance, showroomDisc, brandDisc, addDisc, onRoad, mileage] = cols;
+
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>
+          <img src="\${brandImg}" width="40"><br>
+          <img src="\${modelImg}" width="100">
+        </td>
+        <td>\${brand}</td>
+        <td>\${model}</td>
+        <td>\${exShow}</td>
+        <td>\${rto}</td>
+        <td>\${insurance}</td>
+        <td>\${showroomDisc}</td>
+        <td>\${brandDisc}</td>
+        <td>\${addDisc}</td>
+        <td>\${onRoad}</td>
+        <td>\${mileage}</td>
+      `;
+      tbody.appendChild(tr);
+    });
+  })
+  .catch(err => {
+    console.error('Failed to fetch Google Sheet:', err);
+  });
