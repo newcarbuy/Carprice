e82bede08147693ecf5677fa4c99ac7ffda7fd52
Car Price <gravipatel@gmail.com>
2025-07-05 20:51:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,42 +11,37 @@ fetch(SHEET_CSV_URL)
       const cols = row.split(',');
       if (cols.length < 11) return;
 
-      const [brandImg, modelImg, mileage, exShow, rto, insurance, editinal, showroomDisc, brandDisc, addDisc, onRoad] = cols;
+      const [brandImg, modelImg, mileage, exShow, rto, insurance, editinal, showroomDisc, brandDisc, addDisc, onRoad, brand, model] = cols;
 
       const card = document.createElement('div');
       card.className = 'car-card';
 
       card.innerHTML = `
-  <div class="image-row">
-    <div class="brand-image">
-      <img src="${brandImg}" alt="Brand Image Not Found" width="100">
-      <div class="brand-name">${brand}</div>
-    </div>
-    <div class="model-image">
-      <img src="${modelImg}" alt="Model Image Not Found" width="160">
-      <div class="model-name">${model}</div>
-    </div>
-  </div>
+        <div class="image-row">
+          <div class="brand-image">
+            <img src="${brandImg}" alt="Brand Image Not Found" width="100">
+            <div class="brand-name">${brand}</div>
+          </div>
+          <div class="model-image">
+            <img src="${modelImg}" alt="Model Image Not Found" width="160">
+            <div class="model-name">${model}</div>
+          </div>
+        </div>
 
-  <div class="model-data">
-    <div class="item"><span class="label">Mileage:</span> <span class="value">${mileage}</span></div>
-    <div class="item"><span class="label">Ex-Showroom:</span> <span class="value">${exShow}</span></div>
-    <div class="item"><span class="label">RTO:</span> <span class="value">${rto}</span></div>
-    <div class="item"><span class="label">Insurance:</span> <span class="value">${insurance}</span></div>
-    <div class="item"><span class="label">Editinal:</span> <span class="value">${editinal}</span></div>
-
-    <hr class="divider">
-
-    <div class="item"><span class="label">Showroom Discount:</span> <span class="value">${showroomDisc}</span></div>
-    <div class="item"><span class="label">Brand Discount:</span> <span class="value">${brandDisc}</span></div>
-    <div class="item"><span class="label">Additional Discount:</span> <span class="value">${addDisc}</span></div>
-    
-    <hr class="divider">
-    
-    <div class="item"><span class="label">On-Road Price:</span> <span class="value">${onRoad}</span></div>
-  </div>
-`;
-
+        <div class="model-data">
+          <div class="item"><span class="label">Mileage:</span> <span class="value">${mileage}</span></div>
+          <div class="item"><span class="label">Ex-Showroom:</span> <span class="value">${exShow}</span></div>
+          <div class="item"><span class="label">RTO:</span> <span class="value">${rto}</span></div>
+          <div class="item"><span class="label">Insurance:</span> <span class="value">${insurance}</span></div>
+          <div class="item"><span class="label">Editinal:</span> <span class="value">${editinal}</span></div>
+          <hr class="divider">
+          <div class="item"><span class="label">Showroom Discount:</span> <span class="value">${showroomDisc}</span></div>
+          <div class="item"><span class="label">Brand Discount:</span> <span class="value">${brandDisc}</span></div>
+          <div class="item"><span class="label">Additional Discount:</span> <span class="value">${addDisc}</span></div>
+          <hr class="divider">
+          <div class="item"><span class="label">On-Road Price:</span> <span class="value">${onRoad}</span></div>
+        </div>
+      `;
 
       container.appendChild(card);
     });
