579a627d0c976d87bab4e3c707e8ba052838d670
Car Price <gravipatel@gmail.com>
2025-07-05 21:22:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,34 @@ fetch(SHEET_CSV_URL)
     const rows = csv.trim().split("\n");
     const data = rows.slice(1).map((row) => row.split(","));
 
+    // Group data by brand (last column is assumed to be brand)
+    const brandGroups = {};
+    data.forEach((d) => {
+      const brand = d[11];
+      if (!brandGroups[brand]) brandGroups[brand] = [];
+      brandGroups[brand].push(d);
+    });
+
     const html = [];
-    html.push("<table>");
-    html.push("<tr><th>Feature</th>" + data.map(d => "<th>" + d[12] + "</th>").join("") + "</tr>"); // Model Names
-    html.push("<tr><td>Image</td>" + data.map(d => "<td><img src='" + d[1] + "' class='model-img'></td>").join("") + "</tr>");
-    html.push("<tr><td>Mileage</td>" + data.map(d => "<td>" + d[2] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>Ex-Showroom</td>" + data.map(d => "<td>" + d[3] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>RTO</td>" + data.map(d => "<td>" + d[4] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>Insurance</td>" + data.map(d => "<td>" + d[5] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>Editinal</td>" + data.map(d => "<td>" + d[6] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>Showroom Discount</td>" + data.map(d => "<td>" + d[7] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>Brand Discount</td>" + data.map(d => "<td>" + d[8] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>Additional Discount</td>" + data.map(d => "<td>" + d[9] + "</td>").join("") + "</tr>");
-    html.push("<tr><td>On-Road Price</td>" + data.map(d => "<td>" + d[10] + "</td>").join("") + "</tr>");
-    html.push("</table>");
+
+    for (const brand in brandGroups) {
+      const models = brandGroups[brand];
+
+      html.push(`<h2>${brand}</h2>`);
+      html.push("<table>");
+      html.push("<tr><th>Feature</th>" + models.map(m => "<th>" + m[12] + "</th>").join("") + "</tr>"); // model names
+      html.push("<tr><td>Image</td>" + models.map(m => "<td><img src='" + m[1] + "' class='model-img'></td>").join("") + "</tr>");
+      html.push("<tr><td>Mileage</td>" + models.map(m => "<td>" + m[2] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>Ex-Showroom</td>" + models.map(m => "<td>" + m[3] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>RTO</td>" + models.map(m => "<td>" + m[4] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>Insurance</td>" + models.map(m => "<td>" + m[5] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>Editinal</td>" + models.map(m => "<td>" + m[6] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>Showroom Discount</td>" + models.map(m => "<td>" + m[7] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>Brand Discount</td>" + models.map(m => "<td>" + m[8] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>Additional Discount</td>" + models.map(m => "<td>" + m[9] + "</td>").join("") + "</tr>");
+      html.push("<tr><td>On-Road Price</td>" + models.map(m => "<td>" + m[10] + "</td>").join("") + "</tr>");
+      html.push("</table><br>");
+    }
 
     document.getElementById("comparison-table").innerHTML = html.join("");
   })
